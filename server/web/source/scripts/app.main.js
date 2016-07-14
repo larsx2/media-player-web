a9ff3a2bbec90252be13ed865d2be6fdf368eff3
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2016-07-14 14:11:39 -0700

    Adding vote notification

--- server/web/source/scripts/app.main.js
+++ server/web/source/scripts/app.main.js
@@ -3,12 +3,18 @@ require('videojs-playlist');
 var $ = require('jquery');
 var _ = require('lodash');
 var handlebars = require('handlebars');
+var noty = require('noty');
 
 $(document).ready(function() {
     var videoList = [];
     var videos = [];
+    var template = handlebars.compile(
+        $('#video-list').html()
+    );
 
-    function loadVideos () {
+
+    function loadVideos (player) {
+        videoList = [];
         $.getJSON('/music/playlist', function(tracks) {
             videos = _.orderBy(tracks, 'votes', 'desc');
             _.each(videos, function(video) {
@@ -19,11 +25,12 @@ $(document).ready(function() {
                     }]
                 });
             });
-            var template = handlebars.compile(
-                $('#video-list').html()
-            );
             var content = template({videos: videos});
             $('#playlist-wrapper').html(content);
+            if (player) {
+                player.playlist(videoList);
+                $('#playlist-wrapper li').first().trigger('click');
+            }
         });
     }
     loadVideos();
@@ -38,7 +45,7 @@ $(document).ready(function() {
             });
 
             //Events
-            $('#playlist li').on('click', function(e) {
+            $('#playlist-wrapper').on('click', 'li', function(e) {
                 e.preventDefault();
 
                 $(this).addClass('playing').siblings().removeClass('playing');
@@ -52,13 +59,12 @@ $(document).ready(function() {
                 player.play();
             });
 
+
             //Listener
             player.on('ended', function() {
                 var songId = $('li.playing').find('a').attr('data-id');
                 $.post("/action/played/" + songId).done(function(songs) {
-                    loadVideos();
-                    player.playlist.first();
-                    //player.play();
+                    loadVideos(player);
                 });
             });
 
@@ -71,11 +77,36 @@ $(document).ready(function() {
 
                         if (votesAdded > 0) {
                             votes.text(video.votes);
+                            notify('+' + votesAdded + ' for ' + video.name);
                         }
                     });
                 });
             }, 1000);
         });
     }, 1000);
+
+    function notify(text, type, layout) {
+        return noty({
+            text: text,
+            type: type || 'success',
+            theme: "relax",
+            dismissQueue: true,
+            layout: layout || 'bottomRight',
+            maxVisible: 5,
+            modal: false,
+            animation: {
+                open: 'animated flipInX',
+                close: 'animated flipOutX'
+            },
+            timeout: 3000,
+            closeWith: ['click', 'hover'],
+            animation: {
+                open: {height: 'toggle'}, // or Animate.css class names like: 'animated bounceInLeft'
+                close: {height: 'toggle'}, // or Animate.css class names like: 'animated bounceOutLeft'
+                easing: 'swing',
+                speed: 250 // opening & closing animation speed
+            },
+        });
+    }
     //videojs.options.flash.swf = "../node_modules/video.js/dist/video-js.swf"
 });
